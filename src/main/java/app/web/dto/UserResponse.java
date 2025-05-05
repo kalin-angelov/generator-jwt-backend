646a16b0fb7bd409bffa3f4958e76ad1b0708e86
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-05 14:06:01 +0300

    make the UserResponse Dto not to return all of the users attributes

--- src/main/java/app/web/dto/UserResponse.java
+++ src/main/java/app/web/dto/UserResponse.java
@@ -1,14 +1,22 @@
 package app.web.dto;
 
-import app.user.model.User;
+import app.user.model.UserRole;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.UUID;
 
 @Data
 @Builder
 public class UserResponse {
 
-    private int status;
-    private User user;
+    private UUID id;
+    private String email;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String imgUrl;
+    private UserRole role;
+    private boolean isActive;
 
 }
