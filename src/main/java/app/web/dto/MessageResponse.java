aca1cbd048b8d5447d3f78d5cbe0db9224e25405
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-07 15:11:14 +0300

    Add token attribute

--- src/main/java/app/web/dto/MessageResponse.java
+++ src/main/java/app/web/dto/MessageResponse.java
@@ -1,13 +1,18 @@
 package app.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageResponse {
 
     private int status;
     private boolean successful;
     private String message;
+    private String token;
 }
