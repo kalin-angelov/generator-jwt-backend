0310b1150972e5a136df0fecedbe2ee61a5cede6
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-05-11 12:13:11 +0300

    add two more annotations

--- src/main/java/app/web/dto/RegisterRequest.java
+++ src/main/java/app/web/dto/RegisterRequest.java
@@ -1,10 +1,14 @@
 package app.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
 
     private String email;
