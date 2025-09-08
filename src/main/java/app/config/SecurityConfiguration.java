f3449b482287a856d8433fd5e146942a6ac1767f
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-08 08:51:50 +0300

    edit: add Authentication provider

--- src/main/java/app/config/SecurityConfiguration.java
+++ src/main/java/app/config/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package app.config;
 
 import app.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,10 +15,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
     @Autowired
     private JwtFilter jwtFilter;
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
 
    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -33,6 +39,7 @@ public class SecurityConfiguration {
                .sessionManagement(session -> session
                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                )
+               .authenticationProvider(authenticationProvider)
                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
        return http.build();
