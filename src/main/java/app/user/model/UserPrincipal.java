a0a2b925286447284d004da54be3726a65546e11
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-08 08:51:01 +0300

    edit: add SimpleGrantedAuthority to the getAuthority method

--- src/main/java/app/user/model/UserPrincipal.java
+++ src/main/java/app/user/model/UserPrincipal.java
@@ -2,6 +2,7 @@ package app.user.model;
 
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -18,7 +19,7 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
     }
 
     @Override
