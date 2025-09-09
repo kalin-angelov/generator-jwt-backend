4d2edc02f73aec58279b256f0919a616bf60e65d
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-09 21:59:31 +0300

    create: create a dto for password change

---
+++ src/main/java/app/web/dto/ChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package app.web.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ChangePasswordRequest {
+
+    private String oldPassword;
+    private String newPassword;
+    private String conferPassword;
+}
