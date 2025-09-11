53685895f8b3423cb04a489fe830c96741a51160
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-11 14:39:12 +0300

    edit: add logout, logout endpoint and logout handler

--- src/main/java/app/config/SecurityConfiguration.java
+++ src/main/java/app/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package app.config;
 
 import app.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -10,19 +9,19 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    @Autowired
-    private JwtFilter jwtFilter;
-
-    @Autowired
-    private AuthenticationProvider authenticationProvider;
+    private final JwtFilter jwtFilter;
+    private final LogoutHandler logoutHandler;
+    private final AuthenticationProvider authenticationProvider;
 
    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,13 +33,22 @@ public class SecurityConfiguration {
                        .requestMatchers("/api/v1/auth/**").permitAll()
                        .anyRequest().authenticated()
                )
-//               .formLogin(Customizer.withDefaults())
                .httpBasic(Customizer.withDefaults())
                .sessionManagement(session -> session
                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                )
                .authenticationProvider(authenticationProvider)
-               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+               .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+               .logout((logout) -> logout
+                       .logoutUrl("/api/v1/auth/logout")
+                       .addLogoutHandler(logoutHandler)
+                       .logoutSuccessHandler(
+                               ((request, response, authentication) ->
+                                       SecurityContextHolder.clearContext()
+                               )
+                       )
+               );
+
 
        return http.build();
    }
