d1480ce43ef5b457d4fccdc553f7ad35d954a661
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-11 14:35:37 +0300

    create: Token entity

---
+++ src/main/java/app/token/model/Token.java
@@ -0,0 +1,35 @@
+package app.token.model;
+
+import app.user.model.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType;
+
+    private boolean revoked;
+
+    private boolean expired;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User owner;
+}
---
+++ src/main/java/app/token/model/TokenType.java
@@ -0,0 +1,5 @@
+package app.token.model;
+
+public enum TokenType {
+    BEARER
+}
